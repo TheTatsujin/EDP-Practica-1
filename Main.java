d237825b441e991ac604aaec6b2eb1872a7c759b
TheTatsujin <luis.martin119@alu.ulpgc.es>
2024-02-21 17:43:01 +0000

    Testing Passed

--- Main.java
+++ Main.java
@@ -3,5 +3,16 @@ import edp.Dictionary;
 public class Main {
     public static void main(String[] args) {
         Dictionary pyDict = new Dictionary();
+        Object key;
+        key = "aaa";
+        pyDict.put(key, 1);
+        pyDict.put(key + "!ac", 2);
+        Object value = pyDict.get(key);
+        System.out.println(value);
+
+        value = pyDict.get(key + "!ac");
+        System.out.println(value);
+        System.out.println(pyDict);
+
     }
 }
--- edp/Dictionary.java
+++ edp/Dictionary.java
@@ -1,5 +1,7 @@
 package edp;
 
+import java.security.KeyException;
+
 public class Dictionary{
     private Object[] keys;
     private Object[] values;
@@ -7,7 +9,6 @@ public class Dictionary{
     private final static int PERTURB_SHIFT = 5;
     private final static int MIN_SIZE = 8;
     private int used;
-
     private int capacity;
 
     public Dictionary(){
@@ -17,29 +18,54 @@ public class Dictionary{
         capacity = MIN_SIZE;
     }
 
-    public void put(Object key, Object value) throws IndexOutOfBoundsException{
-        int index = this.generateIndex(key, false);
-        if (index == -1){
-            throw new IndexOutOfBoundsException();
+    public void put(Object key, Object value){
+        if (this.contains(key)){
+            // Ya existe esa Clave
+            return;
         }
+        int index = this.generateIndex(key);
+        if (index == -1){
+            // FULL!
+            return;
+        }
+
         keys[index] = key;
         values[index] = value;
         used++;
-        if (used >= LOAD_FACTOR(used)){
+        if (used >= LOAD_FACTOR(capacity)){
+            //System.out.println("RESIZE!!");
             this.resize();
         }
     }
 
     public Object get(Object key){
-        int index = this.generateIndex(key, true);
-        if (index == -1){
+        if (this.isEmpty()){
+            // TODO: Exception isEmpty
             return null;
         }
-        return this.values[index];
+        int perturb = key.hashCode();
+        int index = perturb & (this.capacity - 1);
+        if (this.keys[index] != null && this.keys[index].equals(key)){
+            return this.values[index];
+        }
+        int iteration = 0;
+        while (this.keys[index] != null && !this.keys[index].equals(key) && iteration < capacity){
+            int j = index;
+            j = 5*j + 1 + perturb;
+            perturb >>= PERTURB_SHIFT;
+            index = j & (this.capacity - 1);
+            if (this.keys[index] != null && this.keys[index].equals(key)){
+                return this.values[index];
+            }
+            iteration++;
+        }
+
+        // No existe
+        return null;
     }
 
     public boolean remove(Object key){
-        int index = this.generateIndex(key, true);
+        int index = this.generateIndex(key);
         return index == 0;
     }
 
@@ -49,10 +75,37 @@ public class Dictionary{
 
 
     private void resize(){
-        //
+
+        int newSize;
+        int oldSize = this.capacity;
+        Object[] oldKeys = this.keys;
+        Object[] oldValues = this.values;
+
+        // Encontrar nueva capacidad
+        for (newSize = MIN_SIZE;
+             newSize <= used && newSize > 0;
+             newSize = GROWTH_RATE(newSize));
+        if (newSize <= 0){
+            // No more memory error
+            return;
+        }
+        this.capacity = GROWTH_RATE(this.capacity);
+        //System.out.println("New SIZE: " + this.capacity);
+
+        // Refill table
+        this.keys = new Object[this.capacity];
+        this.values = new Object[this.capacity];
+
+        for (int j=0; j < oldSize; j++){
+            if (oldKeys[j] != null){
+                int newIndex = this.generateIndex(oldKeys[j]);
+                this.keys[newIndex] = oldKeys[j];
+                this.values[newIndex] = oldValues[j];
+            }
+        }
     }
-    private static int LOAD_FACTOR(int used){
-        return (used << 1) / 3;
+    private static int LOAD_FACTOR(int capacity){
+        return ((capacity << 1) / 3) + 1;
     }
 
     private static int GROWTH_RATE(int capacity){
@@ -61,42 +114,76 @@ public class Dictionary{
 
     private boolean contains(Object key){
         int perturb = key.hashCode();
-        int index = perturb % this.capacity;
-        if (this.keys[index].equals(key)){
+        int index = perturb & (this.capacity - 1);
+
+        if (this.isEmpty()){
+            return false;
+        }
+
+        if (this.keys[index] != null && this.keys[index].equals(key)){
             return true;
         }
-        while (!this.keys[index].equals(key)){
+        int iteration = 0;
+        while (this.keys[index] != null && !this.keys[index].equals(key) && iteration < capacity){
             int j = index;
             j = 5*j + 1 + perturb;
             perturb >>= PERTURB_SHIFT;
-            index = j % this.capacity;
-            if (this.keys[index].equals(key)){
+            index = j & (this.capacity - 1);
+            if (this.keys[index] != null && this.keys[index].equals(key)){
                 return true;
             }
+            iteration++;
         }
         return false;
 
     }
-    public int generateIndex(Object key, boolean isSearching){
-        Object searchingKey = null;
-        if (isSearching){
-            searchingKey = key;
-        }
+    private int generateIndex(Object key){
+        /*
+        *   Algoritmo que usa Python para manejo de colision
+        *   Funciona mejor para capacity = 2**i;
+        *
+        * */
         int perturb = key.hashCode();
-        int index = perturb % this.capacity;
+        int index = perturb & (this.capacity - 1);
 
-        if (!this.contains(key)){
-            return -1;
-        }
-
-        while (this.keys[index] != searchingKey){
-            System.out.println("Collision!");
+        int iterations = 0;
+        while (this.keys[index] != null){
+            if (iterations >= capacity){
+                return -1;
+            }
+            //System.out.println("Collision!");
             int j = index;
             j = 5*j + 1 + perturb;
             perturb >>= PERTURB_SHIFT;
-            index = j % this.capacity;
+            index = j & (this.capacity - 1);
+            iterations++;
         }
         return index;
+    }
+
+    public boolean isEmpty(){
+        return this.used == 0;
+    }
+
+    @Override
+    public String toString(){
+        String result = "{";
+        int i = 0;
+        int j = 0;
+        while (used > 0 && i < used && j < capacity){
+
+            if (keys[j] != null){
+                if (i > 0){
+                    result += ", ";
+                }
+                result += keys[j] + " : " + values[j];
+                i++;
+            }
+
+
+            j++;
+        }
+        return result + "}";
     }
 
 
